94f0d1312024ad07b1fd2a3f3debf3904f4e2295
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-06 00:00:00 +0300

    bin search revsersive

---
+++ binary_search/recursive.py
@@ -0,0 +1,13 @@
+def binary_search(arr, el, low, heigh):
+    if low > heigh: return False
+    mid = int((low + heigh) / 2)
+    if arr[mid] == el: return True
+    if arr[mid] > el: return binary_search(arr, el, low, mid - 1)
+    else: return binary_search(arr, el, mid + 1, heigh)
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(binary_search(arr, 10, 0, len(arr) - 1))
+print(binary_search(arr, 1, 0, len(arr) - 1))
+print(binary_search(arr, -1, 0, len(arr) - 1))
+print(binary_search(arr, 3232, 0, len(arr) - 1))
+
